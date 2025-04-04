646ccf73f6c6d4dc4459ce98e84ecf31cba9c382
jdai1 <julian.dai@gmail.com>
2025-04-04 11:06:38 -0400

    stencil rearrangement

--- java/compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-########################################
-############# CSCI 2951-O ##############
-########################################
-
-# Update this file with instructions on how to compile your code
-javac -classpath /local/projects/cplex/CPLEX_Studio221/cplex/lib/cplex.jar ./src/solver/lp/*.java
--- java/localcompile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-########################################
-############# CSCI 2951-O ##############
-########################################
-
-# Update this file with instructions on how to compile your code
-javac -classpath /Applications/CPLEX_Studio2211/cplex/lib/cplex.jar ./src/solver/lp/*.java
